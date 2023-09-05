ee3327c535a72a8c285ce759bf08ae302fd98740
Ymgc19 <yamaguchiyuhei1119@icloud.com>
2023-09-05 09:55:58 +0900

    d

---
+++ pages/2章 章末問題.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import base64
+from datetime import datetime, timedelta
+
+# フルスクリーン
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.set_page_config(
+    page_title="webpage of yama frog",
+    layout="wide", 
+    initial_sidebar_state="auto",
+    page_icon=":paintbrush:"
+)
+
+st.title("2章の章末問題に関して")
+st.markdown("##### 1問目")
+st.markdown("")
